6d56a0631414312a0dd3ffd6e2737367dc2ffbd9
Baiker000 <sasha.orc.baiker@gmail.com>
2017-07-06 02:45:41 +0300

    mission 2

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
 def wsgi_application(env, start_response):
 	status ='200 OK'
 	headers=[('Content-Type', 'text/plain')]
-	q_string= env.get('QUERY_STRING')
-	q=q_string.split('&')
-	body=''
-	for i in q:
-		body+=i + '/n'
+	q_string= str(env.get('QUERY_STRING'))
+	body=q_string.replace('&', '\n')
+	body=body.encode()
 	start_response(status, headers)
-	return [body.strip().encode()]
+	return [body]
