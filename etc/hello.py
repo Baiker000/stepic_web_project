d1c392ad747540c395c7e65fd5e2d251d7f11256
Baiker000 <sasha.orc.baiker@gmail.com>
2017-07-06 23:02:54 +0300

    mission 3

--- etc/hello.py
+++ etc/hello.py
@@ -1,3 +1,4 @@
-bind = '0.0.0.0:8080'
+bind = '0.0.0.0:8000'
 workers = 3
 user = "nobody"
+pythonpath = '/home/box/web/ask'
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo gunicorn -c /home/box/web/etc/hello.py ask.wsgi:application
