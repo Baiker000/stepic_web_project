f40abb8a5431da910ea81df85e0111203cff5511
Baiker000 <sasha.orc.baiker@gmail.com>
2017-07-27 02:04:05 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -c /home/box/web/etc/hello.py ask.wsgi:application
-sudo /etc/init/d/mysql start
+sudo /etc/init.d/mysql start
 sudo mysql -uroot -e "create database ask"
